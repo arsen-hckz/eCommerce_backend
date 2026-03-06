4c438edfcb55c9ce492224568e33ab3a68eba682
arsen-hckz <yiannis.arsenidis@gmail.com>
2026-03-06 17:02:23 +0200

    user admin problem fix

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ["email", "username", "is_staff", "is_admin"]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("phone", "address", "is_admin")}),
+    )
+    ordering = ["email"]
+
+admin.site.register(User, CustomUserAdmin)
