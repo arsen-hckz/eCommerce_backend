56a5c34d9a061023851653758da5ed19a89e283e
arsen-hckz <yiannis.arsenidis@gmail.com>
2026-03-19 14:07:03 +0200

    fix missing serializer import

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, UserSerializer
 import threading
 import os
 
